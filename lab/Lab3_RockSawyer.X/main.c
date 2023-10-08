261f9e599f08cdfbca390e390ae7190c04156a22
Rock Sawyer <202010408@cegepsherbrooke.qc.ca>
2023-10-08 10:50:24 -0300

    lab3 1er commit apres avoir déplacé le dossier lab3. Le LED demandé allume comme prévue.

---
+++ lab/Lab3_RockSawyer.X/main.c
@@ -0,0 +1,138 @@
+ /**
+ * @file   main.c, projet Lab1
+ * @author 
+ * @date   
+ * @brief    
+ * @version 1.0
+ * Environnement:
+ *     Développement: MPLAB X IDE (version 6.05)
+ *     Compilateur: XC8 (version 2.40)
+ *     Matériel: Carte démo du Pickit4. PIC 18F45K20
+ */
+
+#include <xc.h>
+#include <stdbool.h>  // pour l'utilisation du type bool
+#include "serie.h"
+
+
+#define _XTAL_FREQ 1000000 //Constante utilisée par __delay_ms(x). Doit = fréq interne du uC
+//#define LIGNE true
+//#define COLONNE false
+#define GAUCHE 0
+#define HAUT 1
+#define DROITE 2
+#define BAS 3
+#define FLECHE_G 'A'
+#define FLECHE_H 'W'
+#define FLECHE_D 'D'
+#define FLECHE_B 'S'
+#define NBLIGNE 5
+#define NBCOLONNE 5
+#define PORT_COLONNE PORTC
+//#define PORT_LIGNE PORTB
+
+void initialisation(void); //fonction qui initialise le pic
+//void afficheDe(int chiffre, int deGouD);    //fonction affichant le dé
+void setDel(int ligne, int colonne);   //fct qui affiche la DEL 
+//void
+/*void testMatrice(void);
+bool deplace(int sens, int* ptrLigne, int* ptrColonne);
+void affichePerdu(void);
+void getSens(int *nouveauSens);*/
+
+void main(void)
+{
+    initialisation();
+    //testMatrice();
+    /*int sens;
+    int ligne;
+    int colonne;*/
+    //int a = 1;
+    //int b = 1;
+    setDel(1, 1);
+    //PORTB = 0b00000010;
+    //PORTC = 0b11111100;
+    NOP();
+   
+    
+    
+    while(1) //boucle principale
+    {   
+        
+        /*int i = 0;
+        int j = 0;
+        for(i=0; i<5 ;i++)  //1re boucle qui brasse le dé de gauche
+        {
+            
+            //setDel(i, a);
+            __delay_ms(500);
+            
+        
+            for(j=0; j<5; j++)  //2e boucle pour le dé de droite
+            {
+                setDel(i, j);
+                __delay_ms(500);
+            }
+           
+        }*/    
+            
+        //PORTDbits.RD7 = PORTDbits.RD7 ^ 1; //On change l'état de la DEL 0
+        //__delay_ms(300); //pour laisser le temps de voir la DEL à son nouvel état
+        
+        
+     }
+}
+
+
+/**
+ * @brief Initialisation des différents regesitres du PIC
+ * @param Aucun
+ * @return Aucun
+ */
+void initialisation(void)
+{
+    TRISB = 0; //Tout le port B en sortie
+    TRISC = 0; //tout le port C en sotie
+
+    //Attention! 
+    ANSELH = 0;  // RB0 à RB4 en mode digital. Sur 18F45K20 AN et PortB sont sur les memes broches
+    //TRISB = 0xFF; //tout le port B en entree
+   
+    PORTB = 0; //DEL du port B tous éteintes
+    PORTC = 1; //DEL du port C tous éteintes
+}
+
+/**
+ * @brief affiche secantiellement les valeur de 1 a 6 pour chacun des de
+ * @param int chiffre qui affiche le chiffre de 1 a 6
+ * @param int deGouD qui selectionne le de de droite ou de gauche
+ * @return Aucun
+ */
+/*void afficheDe(int chiffre, int deGouD)
+{
+    int tableauDe[6] = {7,11,3,10,2,8};
+    
+    PORTC = tableauDe[chiffre];
+    PORTDbits.RD0 = deGouD;
+    
+    
+}*/
+
+/**
+ * @brief affiche secantiellement chaque DEL de la matrice
+ * @param int ligne qui sera la serie de DEL sur l'axe des x
+ * @param int colonne qui sera la série de DEL sur l'axe des y
+ * @return Aucun
+ */
+void setDel(int ligne, int colonne)
+{
+    //int tempLi = 0;
+    //int tempCo = 0;
+    int tableauLi[6] = {0, 12, 11, 2, 9, 4};
+    int tableauCo[6] = {0, 1, 3, 10, 7, 8};
+    
+    
+    PORTB = tableauLi[ligne];
+    PORTC = tableauCo[colonne];
+    //PORTDbits.RD0 = deGouD;
+}
---
+++ lab/Lab3_RockSawyer.X/nbproject/Makefile-Mini_board_config.mk
@@ -0,0 +1,172 @@
+#
+# Generated Makefile - do not edit!
+#
+# Edit the Makefile in the project folder instead (../Makefile). Each target
+# has a -pre and a -post target defined where you can add customized code.
+#
+# This makefile implements configuration specific macros and targets.
+
+
+# Include project Makefile
+ifeq "${IGNORE_LOCAL}" "TRUE"
+# do not include local makefile. User is passing all local related variables already
+else
+include Makefile
+# Include makefile containing local settings
+ifeq "$(wildcard nbproject/Makefile-local-Mini_board_config.mk)" "nbproject/Makefile-local-Mini_board_config.mk"
+include nbproject/Makefile-local-Mini_board_config.mk
+endif
+endif
+
+# Environment
+MKDIR=gnumkdir -p
+RM=rm -f 
+MV=mv 
+CP=cp 
+
+# Macros
+CND_CONF=Mini_board_config
+ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+IMAGE_TYPE=debug
+OUTPUT_SUFFIX=elf
+DEBUGGABLE_SUFFIX=elf
+FINAL_IMAGE=${DISTDIR}/Lab3_RockSawyer.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+else
+IMAGE_TYPE=production
+OUTPUT_SUFFIX=hex
+DEBUGGABLE_SUFFIX=elf
+FINAL_IMAGE=${DISTDIR}/Lab3_RockSawyer.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+endif
+
+ifeq ($(COMPARE_BUILD), true)
+COMPARISON_BUILD=-mafrlcsj
+else
+COMPARISON_BUILD=
+endif
+
+# Object Directory
+OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
+
+# Distribution Directory
+DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
+
+# Source Files Quoted if spaced
+SOURCEFILES_QUOTED_IF_SPACED=main.c serie.c
+
+# Object Files Quoted if spaced
+OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/main.p1 ${OBJECTDIR}/serie.p1
+POSSIBLE_DEPFILES=${OBJECTDIR}/main.p1.d ${OBJECTDIR}/serie.p1.d
+
+# Object Files
+OBJECTFILES=${OBJECTDIR}/main.p1 ${OBJECTDIR}/serie.p1
+
+# Source Files
+SOURCEFILES=main.c serie.c
+
+
+
+CFLAGS=
+ASFLAGS=
+LDLIBSOPTIONS=
+
+############# Tool locations ##########################################
+# If you copy a project from one host to another, the path where the  #
+# compiler is installed may be different.                             #
+# If you open this project with MPLAB X in the new host, this         #
+# makefile will be regenerated and the paths will be corrected.       #
+#######################################################################
+# fixDeps replaces a bunch of sed/cat/printf statements that slow down the build
+FIXDEPS=fixDeps
+
+.build-conf:  ${BUILD_SUBPROJECTS}
+ifneq ($(INFORMATION_MESSAGE), )
+	@echo $(INFORMATION_MESSAGE)
+endif
+	${MAKE}  -f nbproject/Makefile-Mini_board_config.mk ${DISTDIR}/Lab3_RockSawyer.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+
+MP_PROCESSOR_OPTION=18F45K20
+# ------------------------------------------------------------------------------------
+# Rules for buildStep: compile
+ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+${OBJECTDIR}/main.p1: main.c  nbproject/Makefile-${CND_CONF}.mk 
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/main.p1.d 
+	@${RM} ${OBJECTDIR}/main.p1 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c  -D__DEBUG=1  -mdebugger=none   -mdfp="${DFP_DIR}/xc8"  -fshort-double -fshort-float -memi=wordwrite -O0 -fasmfile -Og -flocal -maddrqual=ignore -xassembler-with-cpp -mwarn=0 -Wa,-a -DXPRJ_Mini_board_config=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file  -ginhx32 -Wl,--data-init -mno-keep-startup -mno-download -mdefault-config-bits $(COMPARISON_BUILD)  -std=c90 -gdwarf-3 -mstack=compiled:auto:auto:auto     -o ${OBJECTDIR}/main.p1 main.c 
+	@-${MV} ${OBJECTDIR}/main.d ${OBJECTDIR}/main.p1.d 
+	@${FIXDEPS} ${OBJECTDIR}/main.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
+	
+${OBJECTDIR}/serie.p1: serie.c  nbproject/Makefile-${CND_CONF}.mk 
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/serie.p1.d 
+	@${RM} ${OBJECTDIR}/serie.p1 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c  -D__DEBUG=1  -mdebugger=none   -mdfp="${DFP_DIR}/xc8"  -fshort-double -fshort-float -memi=wordwrite -O0 -fasmfile -Og -flocal -maddrqual=ignore -xassembler-with-cpp -mwarn=0 -Wa,-a -DXPRJ_Mini_board_config=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file  -ginhx32 -Wl,--data-init -mno-keep-startup -mno-download -mdefault-config-bits $(COMPARISON_BUILD)  -std=c90 -gdwarf-3 -mstack=compiled:auto:auto:auto     -o ${OBJECTDIR}/serie.p1 serie.c 
+	@-${MV} ${OBJECTDIR}/serie.d ${OBJECTDIR}/serie.p1.d 
+	@${FIXDEPS} ${OBJECTDIR}/serie.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
+	
+else
+${OBJECTDIR}/main.p1: main.c  nbproject/Makefile-${CND_CONF}.mk 
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/main.p1.d 
+	@${RM} ${OBJECTDIR}/main.p1 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c   -mdfp="${DFP_DIR}/xc8"  -fshort-double -fshort-float -memi=wordwrite -O0 -fasmfile -Og -flocal -maddrqual=ignore -xassembler-with-cpp -mwarn=0 -Wa,-a -DXPRJ_Mini_board_config=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file  -ginhx32 -Wl,--data-init -mno-keep-startup -mno-download -mdefault-config-bits $(COMPARISON_BUILD)  -std=c90 -gdwarf-3 -mstack=compiled:auto:auto:auto     -o ${OBJECTDIR}/main.p1 main.c 
+	@-${MV} ${OBJECTDIR}/main.d ${OBJECTDIR}/main.p1.d 
+	@${FIXDEPS} ${OBJECTDIR}/main.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
+	
+${OBJECTDIR}/serie.p1: serie.c  nbproject/Makefile-${CND_CONF}.mk 
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/serie.p1.d 
+	@${RM} ${OBJECTDIR}/serie.p1 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c   -mdfp="${DFP_DIR}/xc8"  -fshort-double -fshort-float -memi=wordwrite -O0 -fasmfile -Og -flocal -maddrqual=ignore -xassembler-with-cpp -mwarn=0 -Wa,-a -DXPRJ_Mini_board_config=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file  -ginhx32 -Wl,--data-init -mno-keep-startup -mno-download -mdefault-config-bits $(COMPARISON_BUILD)  -std=c90 -gdwarf-3 -mstack=compiled:auto:auto:auto     -o ${OBJECTDIR}/serie.p1 serie.c 
+	@-${MV} ${OBJECTDIR}/serie.d ${OBJECTDIR}/serie.p1.d 
+	@${FIXDEPS} ${OBJECTDIR}/serie.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
+	
+endif
+
+# ------------------------------------------------------------------------------------
+# Rules for buildStep: assemble
+ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+else
+endif
+
+# ------------------------------------------------------------------------------------
+# Rules for buildStep: assembleWithPreprocess
+ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+else
+endif
+
+# ------------------------------------------------------------------------------------
+# Rules for buildStep: link
+ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+${DISTDIR}/Lab3_RockSawyer.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk  configuration_bits.p1  
+	@${MKDIR} ${DISTDIR} 
+	${MP_CC} $(MP_EXTRA_LD_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -Wl,-Map=${DISTDIR}/Lab3_RockSawyer.X.${IMAGE_TYPE}.map  -D__DEBUG=1  -mdebugger=none  -DXPRJ_Mini_board_config=$(CND_CONF)  -Wl,--defsym=__MPLAB_BUILD=1   -mdfp="${DFP_DIR}/xc8"  -fshort-double -fshort-float -memi=wordwrite -O0 -fasmfile -Og -flocal -maddrqual=ignore -xassembler-with-cpp -mwarn=0 -Wa,-a -msummary=-psect,-class,+mem,-hex,-file  -ginhx32 -Wl,--data-init -mno-keep-startup -mno-download -mdefault-config-bits -std=c90 -gdwarf-3 -mstack=compiled:auto:auto:auto     -mrom=default,-7dc0-7fff -mram=default,-5f4-5ff,-f9c-f9c,-fd4-fd4,-fdb-fdf,-fe3-fe7,-feb-fef,-ffd-fff  $(COMPARISON_BUILD) -Wl,--memorysummary,${DISTDIR}/memoryfile.xml -o ${DISTDIR}/Lab3_RockSawyer.X.${IMAGE_TYPE}.${DEBUGGABLE_SUFFIX}  ${OBJECTFILES_QUOTED_IF_SPACED}    configuration_bits.p1 
+	@${RM} ${DISTDIR}/Lab3_RockSawyer.X.${IMAGE_TYPE}.hex 
+	
+else
+${DISTDIR}/Lab3_RockSawyer.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk  configuration_bits.p1 
+	@${MKDIR} ${DISTDIR} 
+	${MP_CC} $(MP_EXTRA_LD_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -Wl,-Map=${DISTDIR}/Lab3_RockSawyer.X.${IMAGE_TYPE}.map  -DXPRJ_Mini_board_config=$(CND_CONF)  -Wl,--defsym=__MPLAB_BUILD=1   -mdfp="${DFP_DIR}/xc8"  -fshort-double -fshort-float -memi=wordwrite -O0 -fasmfile -Og -flocal -maddrqual=ignore -xassembler-with-cpp -mwarn=0 -Wa,-a -msummary=-psect,-class,+mem,-hex,-file  -ginhx32 -Wl,--data-init -mno-keep-startup -mno-download -mdefault-config-bits -std=c90 -gdwarf-3 -mstack=compiled:auto:auto:auto     $(COMPARISON_BUILD) -Wl,--memorysummary,${DISTDIR}/memoryfile.xml -o ${DISTDIR}/Lab3_RockSawyer.X.${IMAGE_TYPE}.${DEBUGGABLE_SUFFIX}  ${OBJECTFILES_QUOTED_IF_SPACED}    configuration_bits.p1 
+	
+endif
+
+
+# Subprojects
+.build-subprojects:
+
+
+# Subprojects
+.clean-subprojects:
+
+# Clean Targets
+.clean-conf: ${CLEAN_SUBPROJECTS}
+	${RM} -r ${OBJECTDIR}
+	${RM} -r ${DISTDIR}
+
+# Enable dependency checking
+.dep.inc: .depcheck-impl
+
+DEPFILES=$(wildcard ${POSSIBLE_DEPFILES})
+ifneq (${DEPFILES},)
+include ${DEPFILES}
+endif
